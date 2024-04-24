95d8b22d2201e60952d83154f6ae1be1f55fa2f5
Jagadeesh985 <monangijagadeesh647@gmail.com>
2024-04-24 17:00:59 +0530

    Spring App initialized

---
+++ com.jagadeesh.app/src/test/java/app/jagadeesh/spring/springHandson/ApplicationTests.java
@@ -0,0 +1,13 @@
+package app.jagadeesh.spring.springHandson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
